de7462e13a392ac6b889f0093b903dfc52365cdb
eecmanny <130661353+eecmanny@users.noreply.github.com>
2024-09-12 06:57:23 -0400

    Update TODO.cs

--- TODO.cs
+++ TODO.cs
@@ -1,1 +1,36 @@
+//Intro to program
+Console.WriteLine("Hello!");
+Console.WriteLine("");
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all todos");
+Console.WriteLine("[A]dd a todo");
+Console.WriteLine("[R]emove a todo");
+Console.WriteLine("[Exit]");
+
+string userMenuChoice = Console.ReadLine();
+
+string TodoMenuChoices(string userMenuChoice)
+{
+    switch (userMenuChoice)
+    {
+        case "S":
+        case "s":
+            //return SeeAllTodo();
+            return "placeholder";
+        case "A":
+        case "a":
+            //return AddATodo();
+            return "placeholder";
+        case "R":
+        case "r":
+            //return RemoveTodo();
+            return "placeholder";
+        case "E":
+        case "e":
+            //return Exit();
+            return "placeholder";
+    }
+       
+}
+Console.ReadKey();
 
