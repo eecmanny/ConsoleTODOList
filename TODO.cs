9e47819d97cde4291f8134b635bf06451458230d
eecmanny <130661353+eecmanny@users.noreply.github.com>
2024-09-17 06:58:29 -0400

    Update TODO.cs

--- TODO.cs
+++ TODO.cs
@@ -1,22 +1,5 @@
-////Might possibly use so menu shows up and then disapears depending on person choices
-//do
-//{
-//    //Intro to program
-//    Console.WriteLine("Hello!");
-////need a continue
-//    Console.WriteLine("");
-//    Console.WriteLine("What do you want to do?");
-//    Console.WriteLine("[S]ee all todos");
-//    Console.WriteLine("[A]dd a todo");
-//    Console.WriteLine("[R]emove a todo");
-//    Console.WriteLine("[E]xit");
-//} while(userMenuChoice != inputChoice);
-
-List<string> todoList = new List<string>();
-//List<string> todoList = new List<string> { "1", "2", "3" };
-
-
-//Console.WriteLine(todoList.Count);
+//List<string> todoList = new List<string>();
+List<string> todoList = new List<string> { "T1", "T2", "T3" };
 
 //Intro to program
 Console.WriteLine("Hello!");
@@ -30,94 +13,72 @@ Console.WriteLine("[E]xit");
 string userMenuChoice = Console.ReadLine();
 
 //executes TodoMenuChoices method
-TodoMenuChoices(userMenuChoice);
+//Need to pass through 2 parameter to work
+string userPickedResults = TodoMenuChoices(userMenuChoice, todoList);
+// You need this to see what is returns from the switch statment 
+Console.WriteLine(userPickedResults);
 
-
-////all the methods need to return strings
-////For some reason switch doesn't work had to use if/esle instead
-//string TodoMenuChoices(string userMenuChoice)
-//{
-//    switch (userMenuChoice)
-//    {
-//        case "S":
-//        case "s":
-//            return SeeAllTodo();
-//        case "A":
-//        case "a":
-//            return AddTodo();
-//        case "R":
-//        case "r":
-//            return RemoveTodo();
-//        case "E":
-//        case "e":
-//            return Exit();
-//        default:
-//            return "Invalid option. Please choose S, A, R, or E.";
-//    }
-//}
-
-//all the methods nees to return strings
-string TodoMenuChoices(string userMenuChoice)
+//all the methods need to return strings
+string TodoMenuChoices(string userPickedResults, List<string> todoList)
 {
-    if (userMenuChoice == "S" || userMenuChoice == "s")
+    switch (userPickedResults)
     {
-        //Console.WriteLine("Displaying all todos.");
-        return SeeAllTodo(todoList);
+        case "S":
+        case "s":
+            return SeeAllTodo(todoList);
+        case "A":
+        case "a":
+            return AddTodo(todoList);
+        case "R":
+        case "r":
+            return RemoveTodo(todoList);
+        case "E":
+        case "e":
+            return Exit();
+        default:
+            return "Invalid option. Please choose S, A, R, or E.";
     }
-    else if (userMenuChoice == "A" || userMenuChoice == "a")
-    {
-        Console.WriteLine("Added a todo.");
-        return AddTodo();
-    }
-    else if (userMenuChoice == "R" || userMenuChoice == "r")
-    {
-        Console.WriteLine("Removing a todo.");
-        return RemoveTodo();
-    }
-    else if (userMenuChoice == "E" || userMenuChoice == "e")
-    {
-        Console.WriteLine("Exiting the program.");
-        return Exit();
-    }
-    Console.WriteLine("Invalid option. Please choose S, A, R, or E.");
-    return "Invalid option. Please choose S, A, R, or E.";
 }
 
 //Todo Read fetch requests methods
+//string SeeAllTodo(List<string> todoList)
 string SeeAllTodo(List<string> todoList)
 {
-    if (todoList.Count > 0)
+
+    if (todoList.Count == 0)
     {
-        return "if statement works";
+        return "There are no todos.";
     }
-    return "else works";
+
+    //string result = "Here are your todos:\n";
+    //foreach (string todo in todoList)
+    //{
+    //    result += todo + "\n";
+    //}
+    //return result;
+    return "total goes here";
 }
 
-string SeeAllResult = SeeAllTodo(todoList);
-Console.WriteLine(SeeAllResult);
 
-string AddTodo()
+//string AddTodo(List<string> todoList)
+string AddTodo(List<string> todoList)
 {
-    if()
-    {
-
-    }
-    return "Adding a todo.";
+    string userAddTodo = Console.ReadLine();
+    todoList.Add(userAddTodo);
+    return userAddTodo;
 }
 
-//string addResult = AddTodo(todoList);
-//Console.WriteLine(addResult);
 
-string RemoveTodo()
+string RemoveTodo(List<string> todoList)
 {
     return "Removing a todo.";
     //return "Removing" + insertVaribaleToBeDeleted;
 }
 
-//string removeResult = RemoveTodo(todoList);
-//Console.WriteLine(removeResult);
 
 string Exit()
 {
     return "Exiting the program.";
 }
+
+Console.ReadKey();
